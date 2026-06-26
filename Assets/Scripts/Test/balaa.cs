66a6511dcaf7fefc16df81fb8cf8ae2c9551bc3b
azazelid <diegoijover@gmail.com>
2026-06-25 20:47:51 -0600

    torreta

---
+++ Assets/Scripts/Test/balaa.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class balaa : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody rb;
+    public float fuerza;
+    private float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 direction = player.transform.position - transform.position;
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * fuerza;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Test/disparotorreta.cs
+++ Assets/Scripts/Test/disparotorreta.cs
@@ -6,22 +6,31 @@ public class disparotorreta : MonoBehaviour
     public Transform balaPos;
 
     private float timer;
+    private GameObject player;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
+        float distance = Vector2.Distance(transform.position, player.transform.position);
 
-        if(timer > 2)
+        if(distance < 4)
         {
-            timer = 0;
-            shoot();
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
         }
+
+       
     }
 
     void shoot()
